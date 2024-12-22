4b3ac5e9e3ac36dc48b61b18804309819a2b75ce
nikes74 <umka813790@ya.ru>
2024-12-22 04:33:30 +0300

    Дополнительное практическое задание по модулю: "Базовые структуры данных."

--- HomeWorks/Module01/module1hard.py
+++ HomeWorks/Module01/module1hard.py
@@ -31,4 +31,4 @@ names.sort()            # в упорядоченный список
 averagescores.update({names[0]:sum(grades[0])/len(grades[0]), names[1]:sum(grades[1])/len(grades[1])})
 averagescores.update({names[2]:sum(grades[2])/len(grades[2]), names[3]:sum(grades[3])/len(grades[3])})
 averagescores.update({names[4]:sum(grades[4])/len(grades[4])})
-print('Средние баллы учеников -', averagescores)     # Вывод в консоль
+print('Средние баллы учеников -', averagescores)    # Вывод в консоль
